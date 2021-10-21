e09ddc143f308c817bac4bc6c0a63ecb514f6030
Hema35 <cheershema@gmail.com>
2021-10-21 19:03:59 -0400

    responsive grid

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MediaChange, MediaObserver } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+dashboardGridCols: number=4;
+cardColspan: number=2;
+  constructor( private mediaObserver: MediaObserver) { }
 
   ngOnInit(): void {
+
+    //responsive dashboard
+    this.mediaObserver.asObservable().subscribe((media: MediaChange[])=>{
+      if(media.some(mediaChange => mediaChange.mqAlias =="lt-sm"))
+      {
+        this.dashboardGridCols=1;
+        this.cardColspan=1;
+      }
+      else if(media.some(mediaChange=>mediaChange.mqAlias=="lt-lg"))
+      {
+        this.dashboardGridCols=2;
+        this.cardColspan=2;
+      }
+      else{
+        this.dashboardGridCols=4;
+        this.cardColspan=2;
+      }
+    });
   }
 
 }
